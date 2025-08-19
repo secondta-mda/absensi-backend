97469a3b53ea217f39d7c88ab14a9c7dc8b070d2
secondta-mda <secondtaardiansyahw@gmail.com>
2025-08-19 13:24:33 +0700

    update db

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.get('/absen', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server jalan di port ${PORT}`);
 });
