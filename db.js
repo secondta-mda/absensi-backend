543a1abbcf9895943d899949cea266c2e07562ea
secondta-mda <secondtaardiansyahw@gmail.com>
2025-08-22 08:30:19 +0700

    update database

--- db.js
+++ db.js
@@ -1,17 +1,22 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
+const fs = require("fs");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  host: process.env.DB_HOST, // ex: mysql-xxx.aivencloud.com
+  port: process.env.DB_PORT, // ex: 13729
+  user: process.env.DB_USER, // biasanya avnadmin
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME, // defaultdb
+  ssl: {
+    ca: fs.readFileSync("./ca.pem"), // sertifikat dari Aiven
+  },
 });
 
-db.connect(err => {
+db.connect((err) => {
   if (err) {
-    console.error('Koneksi ke DB gagal:', err);
+    console.error("❌ Koneksi ke DB gagal:", err);
   } else {
-    console.log('Terkoneksi ke database MySQL');
+    console.log("✅ Terkoneksi ke database MySQL Aiven");
   }
 });
 
